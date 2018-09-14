5511dcabbed6e8303833b5e1026046e8328961ed
Paulo Belo <pjbelo@gmail.com>
2018-09-14 17:39:05 +0100

    rails update 5.1 to 5.2

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Load environment vars from local file
-local_env = File.join(Rails.root, 'config', 'local_env.rb')
-load(local_env) if File.exists?(local_env)
-
 # Initialize the Rails application.
 Rails.application.initialize!
